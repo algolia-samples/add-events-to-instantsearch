4f7ea52674112499e1e80a5ff5e958bffeac7fb6
Chuck Meyer <chuck.meyer@algolia.com>
2026-01-22 14:16:41 -0500

    Changed title to emphasize agentic flow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
          Gotta Click &apos;em All!
       </h1>
       <p className="header-subtitle">
-        adding Insights to {' '}
+        agentic search with {' '}
         <a href="https://github.com/algolia/react-instantsearch">
           React InstantSearch Hooks
         </a>
